8487fd2e90bfe5084808d770f427590cd419f190
KING D <USER@Kingisdave.Kiingisdave>
2022-09-26 23:21:29 +0100

    Vuetify installed

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(store).use(router).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
