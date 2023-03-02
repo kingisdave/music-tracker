520377ddd2885e3857ace5242d38ca7306865478
kingisdave <davidfayreal@gmail.com>
2023-03-02 04:51:42 +0100

    started with restructuring of error alert

---
+++ client/src/components/globals/ErrorPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="px-1">
+    <v-alert
+      class="small mx-1"
+      theme="dark"
+      v-model="showError"
+      closable
+      close-label="Close Alert"
+      color="#C51162"
+      elevation="2"
+    >
+      <small>{{ error }}</small>
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'title'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
