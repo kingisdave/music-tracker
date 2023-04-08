9d6abb5b311a20d204b80a2cf6455658243dd087
kingisdave <davidfayreal@gmail.com>
2023-04-08 21:07:38 +0100

    uncovered hooks not functioning

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -15,12 +15,13 @@ module.exports = (sequelize, DataTypes) => {
       unique: true
     },
     password: DataTypes.STRING
-  },{
-    associate: function (models) {
-      User.hasOne(models.Bookmark, {
-        onDelete: 'CASCADE'
-      });
-    }
+  // },{
+  //   associate: function (models) {
+  //     User.hasOne(models.Bookmark, {
+  //       onDelete: 'CASCADE',
+  //       hooks: true
+  //     });
+  //   }
   },{
     hooks: {
       beforeCreate: async (user) => {
@@ -28,12 +29,18 @@ module.exports = (sequelize, DataTypes) => {
           const salt = await bcrypt.genSalt(10);
           user.password = await bcrypt.hash(user.password, salt);
         }
+      // },
+      // afterCreate: (bookmark, options) => {
+      //   console.log(`\n\n\n ${options} After update model \n\n\n, ${bookmark}`);
       },
       beforeUpdate:async (user) => {
         if (user.password) {
           const salt = await bcrypt.genSalt(10);
           user.password = await bcrypt.hash(user.password, salt);
         }
+      },
+      afterBulkCreate: (bookmark, options) => {
+        console.log(`\n\n\n ${options} After update model \n\n\n, ${bookmark}`);
       }   
     }   
   });
@@ -41,6 +48,13 @@ module.exports = (sequelize, DataTypes) => {
   User.prototype.validPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
   };
+  // User.associate = function (models) {
+  //   User.hasOne(models.Bookmark, {
+  //     through: Bookmark,
+  //     onDelete: 'CASCADE',
+  //     hooks: true
+  //   });
+  // };
   
   return User;
 }
