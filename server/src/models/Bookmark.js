3f3ae319e9f48990432d3312a21de9074e5b6ffc
kingisdave <davidfayreal@gmail.com>
2023-04-13 15:58:25 +0100

    Constraints=false was the missing solution

--- server/src/models/Bookmark.js
+++ server/src/models/Bookmark.js
@@ -10,30 +10,3 @@ module.exports = (sequelize) => {
 
   return Bookmark;
 }
-
-// module.exports = (sequelize) => {
-//   const Bookmark = sequelize.define('Bookmark', {
-//     // SongId: {
-//     //   type: DataTypes.STRING,
-//     //   allowNull: true
-//     // },
-//     // UserId: {
-//     //   type: DataTypes.STRING,
-//     //   allowNull: true
-//     // }
-//   })
-//   // Bookmark.associate = models => {
-//   //   Bookmark.belongsTo(models.Song)
-//   //   Bookmark.belongsTo(models.User)
-//   // }
-//   Bookmark.associate = function (models) {
-//     Bookmark.belongsTo(models.User, {
-//       foreignKey: 'UserId'
-//     })
-//     Bookmark.belongsTo(models.Song, {
-//       foreignKey: 'SongId'
-//     })
-//   } 
-
-//   return Bookmark;
-// }
