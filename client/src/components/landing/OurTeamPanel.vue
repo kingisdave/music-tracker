5cf54f96c7e02ecce64e016a1cdf6bee10b8438c
kingisdave <davidfayreal@gmail.com>
2023-06-24 18:31:33 +0100

    updating the teampanel

--- client/src/components/landing/OurTeamPanel.vue
+++ client/src/components/landing/OurTeamPanel.vue
@@ -11,24 +11,39 @@
       </h3>
     </v-card-text>
     <v-card
-      theme="dark"
       flat
       rounded="0"
     >
       <v-window v-model="onboarding">
         <v-window-item
-          v-for="n in length"
-          :key="`card-${n}`"
+          v-for="n in (team/length)"
+          :key="`group-${n}`"
           :value="n"
         >
-          <v-card
+        <v-row class="">
+          <v-col cols="6" sm="4" md="3"
+            class="bg-white"
+            v-for="x in 4"
+            :key="`team-${(n-1) * 4 + x}`"
+          >
+            <v-card
+              height="200"
+              class="d-flex justify-center align-center"
+            >
+              <span class="text-h2">
+                Card {{ (n - 1) * 4 + x }}
+              </span>
+            </v-card>
+          </v-col>
+        </v-row>
+          <!-- <v-card
             height="200"
             class="d-flex justify-center align-center"
           >
             <span class="text-h2">
               Card {{ n }}
             </span>
-          </v-card>
+          </v-card> -->
         </v-window-item>
       </v-window>
 
@@ -46,81 +61,29 @@
         ></v-btn>
       </v-card-actions>
     </v-card>
-    <!-- <v-card
-      class="mx-auto"
-      max-width="500"
-    >
-      <v-card-title class="text-h6 font-weight-regular justify-space-between">
-        <span>{{ currentTitle }}</span>
-        <v-avatar
-          color="primary"
-          size="24"
-          v-text="step"
-        ></v-avatar>
-      </v-card-title>
-
-      <v-window v-model="step">
-        <v-window-item :value="1">
-          
-        </v-window-item>
-
-        <v-window-item :value="2">
-          
-        </v-window-item>
-
-        <v-window-item :value="3">
-          
-        </v-window-item>
-      </v-window>
-
-      <v-divider></v-divider>
-
-      <v-card-actions>
-        <v-btn
-          v-if="step > 1"
-          variant="text"
-          @click="step--"
-        >
-          Back
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn
-          v-if="step < 3"
-          color="primary"
-          variant="flat"
-          @click="step++"
-        >
-          Next
-        </v-btn>
-      </v-card-actions>
-    </v-card> -->
-    <!-- <v-row class="">
-      <v-col cols="6" sm="4" md="3"></v-col>
-      <v-col cols="6"></v-col>
-    </v-row> -->
   </v-card>
 </template>
 <script>
 export default {
   data () {
     return {
-      length: 3,
+      team: 20,
       onboarding: 0,
+      length: 4,
       fullname: null,  
-      email: null,
-      phoneNumber: null,
+      title: null,
       message: null  
     }
   },
   methods: {
     next () {
-      this.onboarding = this.onboarding + 1 > this.length
+      this.onboarding = this.onboarding + 1 > (this.team/this.length)
         ? 1
         : this.onboarding + 1
     },
     prev () {
       this.onboarding = this.onboarding - 1 <= 0
-        ? this.length
+        ? (this.team/this.length)
         : this.onboarding - 1
     }
   }
