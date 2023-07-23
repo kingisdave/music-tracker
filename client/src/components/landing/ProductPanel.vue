3d190062e4edaa3d4591b5089dc5727c3aa0335f
kingisdave <davidfayreal@gmail.com>
2023-07-23 20:29:41 +0100

    updating

--- client/src/components/landing/ProductPanel.vue
+++ client/src/components/landing/ProductPanel.vue
@@ -2,15 +2,34 @@
   <v-card class="product-wrapper primary"
     align="center">
     <div class="product-bgcolor"></div>
+    <v-row>
+      <v-col cols="12" sm="6" md="6"
+        class="">
+        <!-- images -->
+      </v-col>
+      <v-col cols="12" sm="6" md="6"
+        class="">
+        <v-card>
+          <v-card-text class="d-block pt-4 pb-6">
+            <h1 class="text-sm-h3 font-weight-medium">
+              Products 
+            </h1>
+            <h3 class="text-subtitle2 text-sm-subtitle mt-3">
+              Product Offering
+            </h3>
+          </v-card-text>
+          <v-card-text class="d-block pt-4 pb-6">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Ullam voluptas iure quibusdam a cupiditate 
+              maiores officia tempora suscipit distinctio autem 
+              odit dolore exercitationem nostrum quo asperiores 
+              optio, repellendus nisi recusandae?
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
     
-    <!-- <v-card-text class="d-block pt-6 pb-8">
-      <h1 class="text-sm-h3 font-weight-medium">
-        Products & Services
-      </h1>
-      <h3 class="text-subtitle2 text-sm-subtitle mt-3">
-        Service Overview
-      </h3>
-    </v-card-text> -->
   </v-card>
   
 </template>
