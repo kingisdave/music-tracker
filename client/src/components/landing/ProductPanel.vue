32bef8e688ce339ab8147a130dda0cd0468c9dc1
kingisdave <davidfayreal@gmail.com>
2023-07-25 14:45:20 +0100

    product panel updated

--- client/src/components/landing/ProductPanel.vue
+++ client/src/components/landing/ProductPanel.vue
@@ -1,53 +1,46 @@
 <template>
-  <v-card class="product-wrapper primary d-flex justify-center align-center"
+  <v-card class="product-wrapper primary d-flex align-center"
     align="center">
     <div class="product-bgcolor primary"></div>
-    <v-row class="h-100 d-flex align-center">
+    <v-row class="d-flex align-center">
+      <v-col cols="12" sm="6" md="6"
+        class="d-block d-sm-none">
+        <v-card class="mx-2 mt-10" variant="none" 
+          style="border: 0 !important;">
+          <v-card-text class="d-block">
+            <h1 class="text-sm-h4 font-weight-medium">
+              Our Products 
+            </h1>
+            <h3 class="text-subtitle2 text-sm-subtitle mt-3">
+              Product Offering
+            </h3>
+          </v-card-text>
+        </v-card>
+      </v-col>
       <v-col cols="12" sm="6" md="6"
         class="d-flex justify-center align-center">
-        <v-row class="ma-7 pa-0 rounded-xl" 
-          style="width: 100%; height: 50vh;">
-          <v-col cols="6" class="">
+        <v-row class="mx-3 ma-sm-5 pa-0 rounded-xl" 
+          style="">
+          <v-col cols="6" 
+            v-for="(image, index) in cardImages"
+            :key="index"
+          >
             <v-card class="ma-1 rounded-xl">
               <v-img
-                src="../../assets/images/nice-guitar.jpg"
+                :src="image.src"
                 aspect-ratio="1"
                 cover
                 class="bg-grey-lighten-2"
-              ></v-img>
-
-            </v-card>
-          </v-col>
-          <v-col cols="6" class="">
-            <v-card class="ma-1 rounded-xl">
-              <v-img
-                src="../../assets/images/tree-house.jpg"
-                aspect-ratio="1"
-                cover
-                class="bg-grey-lighten-2"
-              ></v-img>
-
-            </v-card>
-          </v-col>
-          <v-col cols="6" class="">
-            <v-card class="ma-1 rounded-xl">
-              <v-img
-                src="../../assets/images/human-book.png"
-                aspect-ratio="1"
-                cover
-                class="bg-grey-lighten-2"
-              ></v-img>
-
-            </v-card>
-          </v-col>
-          <v-col cols="6" class="">
-            <v-card class="ma-1 rounded-xl">
-              <v-img
-                src="../../assets/images/earphone.jpg"
-                aspect-ratio="1"
-                cover
-                class="bg-grey-lighten-2"
-              ></v-img>
+              >
+              <!-- <template v-slot:placeholder>
+                <div class="d-flex align-center justify-center fill-height">
+                  <v-progress-circular
+                    color="grey-lighten-4"
+                    indeterminate
+                  ></v-progress-circular>
+                </div>
+              </template> -->
+              </v-img>
 
             </v-card>
           </v-col>
@@ -55,17 +48,18 @@
       </v-col>
       <v-col cols="12" sm="6" md="6"
         class="">
-        <v-card class="">
-          <v-card-text class="d-block pt-4 pb-6">
-            <h1 class="text-sm-h3 font-weight-medium">
+        <v-card class="mx-4" variant="none">
+          <v-card-text class="d-none d-sm-inline pt-4 pb-3 pb-sm-6">
+            <h1 class="text-h4 text-md-h3 font-weight-medium">
               Products 
             </h1>
             <h3 class="text-subtitle2 text-sm-subtitle mt-3">
               Product Offering
             </h3>
           </v-card-text>
-          <v-card-text class="d-block pt-4 pb-6">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+          <v-card-text class="d-block pt-4 mb-6">
+            <p class="text-body-1">
+              Lorem ipsum dolor sit amet consectetur adipisicing 
               elit. Ullam voluptas iure quibusdam a cupiditate 
               maiores officia tempora suscipit distinctio autem 
               odit dolore exercitationem nostrum quo asperiores 
@@ -82,13 +76,30 @@
 
 <script>
 export default {
-  
+  data () {
+    return {
+      cardImages: [
+        {
+          src: require('../../assets/images/nice-guitar.jpg')
+        },
+        {
+          src: require('../../assets/images/tree-house.jpg')
+        },
+        {
+          src: require('../../assets/images/human-book.png')
+        },
+        {
+          src: require('../../assets/images/earphone.jpg')
+        }
+      ]
+    };
+  }
 }
 </script>
 
 <style scoped>
   .product-wrapper {
-    min-height:80vh;
+    min-height:85vh;
     background-image: url('../../assets/images/music-wallpaper.jpg');
     background-position: center;
     background-color: fill;
