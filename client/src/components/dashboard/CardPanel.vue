a40672297692aa0173445f5003bafdaaf4ce68d1
kingisdave <davidfayreal@gmail.com>
2023-08-14 17:53:51 +0100

    updating the card display on dashboard

--- client/src/components/dashboard/CardPanel.vue
+++ client/src/components/dashboard/CardPanel.vue
@@ -19,117 +19,36 @@
                 subtitle="Users"
               />
             </v-col>
-            <!-- <v-col cols="6" md="3"> -->
-              <!-- <single-card
-                :rounded="rounded"
-                card-class="cardOne"
-                icon-size="xx-large"
-                icon="mdi-account-group"
-                title="300K"
-                subtitle="Users"
-              /> -->
-              <!-- <v-card
-                :rounded="rounded"
-                class="px-3 py-4 m-1 cardOne"
-                theme="dark"
-              >
-                <div class="d-flex flex-no-wrap justify-space-between">
-                  <v-avatar
-                    class="ma-2"
-                    size="40"
-                    rounded="0"
-                  >
-                    <v-icon size="xx-large"
-                      icon="mdi-account-group"
-                    ></v-icon>
-                  </v-avatar>
-                  <div>
-                    <v-card-title class="text-xs-h6 text-md-h5">
-                      300K
-                    </v-card-title>
-                    <v-card-subtitle>Users</v-card-subtitle>
-                  </div>
-                </div>
-              </v-card> -->
-            <!-- </v-col> -->
             <v-col cols="6" md="3">
-              <v-card
+              <single-card
                 :rounded="rounded"
-                class="px-3 py-4 m-1 cardTwo"
-                theme="dark"
-              >
-                <div class="d-flex flex-no-wrap justify-space-between">
-                  <v-avatar
-                    class="ma-1"
-                    size="40"
-                    rounded="0"
-                  >
-                    <v-icon size="x-large"
-                      icon="mdi-chart-areaspline-variant"
-                    ></v-icon>
-                  </v-avatar>
-                  <div>
-                    <v-card-title class="text-xs-h6 text-md-h5">
-                      48.59%
-                    </v-card-title>
-                    <v-card-subtitle>Growth</v-card-subtitle>
-                  </div>
-                </div>
-              </v-card>
+                card-class="cardTwo"
+                icon-size="xx-large"
+                icon="mdi-chart-areaspline-variant"
+                title="48.59%"
+                subtitle="Growth"
+              />
             </v-col>
-            <v-col md="3" cols="6">
-              <v-card
+            <v-col cols="6" md="3">
+              <single-card
                 :rounded="rounded"
-                class="px-4 py-4 m-1 cardThree"
-                theme="dark"
-              >
-                <div
-                  class="d-flex flex-no-wrap justify-space-between">
-                  <v-avatar
-                    class="ma-2"
-                    size="40"
-                    rounded="0"
-                  >
-                    <v-icon size="x-large"
-                      icon="mdi-chart-areaspline-variant"
-                    ></v-icon>
-                  </v-avatar>
-                  <div>
-                    <v-card-title class="text-xs-h6 text-md-h5">
-                      1.5M
-                    </v-card-title>
-                    <v-card-subtitle class="text-small">Downloads</v-card-subtitle>
-                  </div>
-                </div>
-              </v-card>
+                card-class="cardThree"
+                icon-size="xx-large"
+                icon="mdi-chart-areaspline-variant"
+                title="1.5M"
+                subtitle="Downloads"
+              />
             </v-col>
-            <v-col md="3" cols="6">
-              <v-card
+            <v-col cols="6" md="3">
+              <single-card
                 :rounded="rounded"
-                class="px-4 py-4 m-1 cardFour"
-                theme="dark"
-              >
-                <div
-                  class="d-flex flex-no-wrap justify-space-between">
-                  <v-avatar
-                    class="ma-2"
-                    size="40"
-                    rounded="0"
-                  >
-                    <v-icon size="x-large"
-                      icon="mdi-chart-areaspline-variant"
-                    ></v-icon>
-                  </v-avatar>
-                  <div>
-                    <v-card-title class="text-xs-h6 text-md-h5">
-                      500K
-                    </v-card-title>
-                    <v-card-subtitle>Stores</v-card-subtitle>
-                  </div>
-                </div>
-              </v-card>
+                card-class="cardFour"
+                icon-size="xx-large"
+                icon="mdi-chart-areaspline-variant"
+                title="500k"
+                subtitle="Stores"
+              />
             </v-col>
-
           </v-row>
         </v-sheet>
       </v-col>
@@ -141,14 +60,13 @@
 import SingleCard from './SingleCard.vue';
 
 export default {
-
-  components: {
-    SingleCard
-  },
   data() {
     return {
       rounded: true,
     };
+  },
+  components: {
+    SingleCard
   }
 }
 </script>
--- client/src/components/dashboard/SingleCard.vue
+++ client/src/components/dashboard/SingleCard.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-card :rounded="rounded" :class="cardClass" theme="dark">
+  <v-card :rounded="rounded" :class="cardClass"
+    class="px-3 py-4 m-1" theme="dark">
     <div class="d-flex flex-no-wrap justify-space-between">
       <v-avatar class="ma-2" size="40" rounded="0">
         <v-icon :size="iconSize" :icon="icon"></v-icon>
       </v-avatar>
       <div>
-        <v-card-title class="text-xs-h6 text-md-h5">{{ title }}</v-card-title>
-        <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
+        <v-card-title class="text-xs-h6 text-md-h5">
+          {{ title }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{ subtitle }}
+        </v-card-subtitle>
       </div>
     </div>
   </v-card>
@@ -14,14 +19,14 @@
 
 <script>
 export default {
-  props: {
-    rounded: boolean,
-    cardClass: string,
-    iconSize: string,
-    icon: string,
-    title: string,
-    subtitle: string,
-  },
+  props: [
+    'rounded',
+    'cardClass',
+    'iconSize',
+    'icon',
+    'title',
+    'subtitle',
+  ]
 };
 </script>
 
